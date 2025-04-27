805fcf27b81398edfd951efbae6f97cb344b9764
houndhollis <houndhollis9797@gmail.com>
2025-04-27 12:00:00 +0900

    main 파일 정리

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,5 @@
-import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
 import App from "./App.tsx"
-import { Provider } from "./app/providers/index.tsx"
-import { queryClient } from "./shared/api/query-client.ts"
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Provider client={queryClient}>
-      <App />
-    </Provider>
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(<App />)

619719320a6682669fd4afa77bc4486050fcb501
houndhollis <houndhollis9797@gmail.com>
2025-04-28 23:36:33 +0900

    feat: queryProvider 추가

---
+++ src/features/post/query/PostQueryProvider.tsx
@@ -0,0 +1,73 @@
+import { createContext, useContext, useState, ReactNode, useMemo } from "react"
+
+interface PostQueryContextType {
+  skip: number
+  setSkip: (skip: number) => void
+  limit: number
+  setLimit: (limit: number) => void
+  searchQuery: string
+  setSearchQuery: (query: string) => void
+  sortBy: string
+  setSortBy: (sortBy: string) => void
+  sortOrder: string
+  setSortOrder: (order: string) => void
+  selectedTag: string
+  setSelectedTag: (tag: string) => void
+}
+
+const PostQueryContext = createContext<PostQueryContextType | undefined>(undefined)
+
+export const usePostControl = () => {
+  const controls = useContext(PostQueryContext)
+  if (!controls) {
+    throw new Error("post 내부에서 사용하세요.")
+  }
+  return controls
+}
+
+interface PostQueryProviderProps {
+  children: ReactNode
+}
+
+export const PostQueryProvider = ({ children }: PostQueryProviderProps) => {
+  const queryParams = new URLSearchParams(window.location.search)
+
+  const [skip, setSkip] = useState(parseInt(queryParams.get("skip") || "0"))
+  const [limit, setLimit] = useState(parseInt(queryParams.get("limit") || "10"))
+  const [searchQuery, setSearchQuery] = useState(queryParams.get("search") || "")
+  const [sortBy, setSortBy] = useState(queryParams.get("sortBy") || "")
+  const [sortOrder, setSortOrder] = useState(queryParams.get("sortOrder") || "asc")
+  const [selectedTag, setSelectedTag] = useState(queryParams.get("tag") || "")
+
+  const memorizedValue = useMemo(
+    () => ({
+      skip,
+      setSkip,
+      limit,
+      setLimit,
+      searchQuery,
+      setSearchQuery,
+      sortBy,
+      setSortBy,
+      sortOrder,
+      setSortOrder,
+      selectedTag,
+      setSelectedTag,
+    }),
+    [
+      skip,
+      setSkip,
+      limit,
+      setLimit,
+      searchQuery,
+      setSearchQuery,
+      sortBy,
+      setSortBy,
+      sortOrder,
+      setSortOrder,
+      selectedTag,
+      setSelectedTag,
+    ],
+  )
+  return <PostQueryContext.Provider value={memorizedValue}>{children}</PostQueryContext.Provider>
+}

c1fd51c38d7c479957d94a1a0705038ae20818c1
houndhollis <houndhollis9797@gmail.com>
2025-05-02 01:40:27 +0900

    feat: vite.config.ts 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,34 @@
-import { defineConfig } from "vite"
+import { defineConfig, Plugin } from "vite"
 import react from "@vitejs/plugin-react"
 import tsconfigPaths from "vite-tsconfig-paths"
 import path from "path"
 
+function apiReplace(): Plugin {
+  return {
+    name: "api-replace",
+    apply: "build",
+    transform(code, id) {
+      if (!id.match(/\.(ts|js|tsx|jsx)$/)) return
+      return code.replace(
+        /(['"`])\/api([^"'`\\]*)\1/g,
+        (_, quote, apiPath) => `${quote}https://dummyjson.com${apiPath}${quote}`,
+      )
+    },
+  }
+}
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), apiReplace()],
+  base: process.env.NODE_ENV === "production" ? "/front_5th_chapter2-3/" : "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+      },
+    },
+    outDir: "dist",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
